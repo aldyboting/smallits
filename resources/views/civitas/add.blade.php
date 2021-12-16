f14b8a2210fcc2984343a02bd2fc8198d3def1d7
serracharisma <serracharismaviontita@yahoo.com>
2021-12-16 17:31:13 +0700

    addcivitas2
    menambahkan isi tambah data civitas

--- resources/views/civitas/add.blade.php
+++ resources/views/civitas/add.blade.php
@@ -146,6 +146,11 @@
                         </div>
                     </div>
 
+                    <div class="flex flex-row-reverse mr-2">
+                        <!--Add button-->
+                        <button class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded" onclick="window.location='{{ route("add-civitas") }}'">Tambah</button>
+                    </div>
+
                 </form>
             </div>
         </div>
